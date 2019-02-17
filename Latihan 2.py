bf55f6c2c6040ae4f851b64e21a107397dcc1a04
bayuadjiepratama <bayuadjiepratama@mhs.pelitabangsa.ac.id>
2019-02-17 09:06:09 +0700

    first commit

---
+++ Latihan 2.py
@@ -0,0 +1,11 @@
+max=0
+while True:
+        a=int(input("Masukkan Bilangan = "))
+        print("")
+        if max < a:
+                max = a
+        print("Bilangan Terbesar = ",max)
+        if a==0:
+                break
+        print("")
+
