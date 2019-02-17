6d70c62d0507ca9b1468d53c7a6971c31e753014
bayuadjiepratama <bayuadjiepratama@mhs.pelitabangsa.ac.id>
2019-02-17 09:47:38 +0700

    first commit

---
+++ Latihan 1.py
@@ -0,0 +1,11 @@
+import random
+
+jumlah = int(input("Masukkan Jumlah n: "))
+
+print()
+
+for i in range  (jumlah):
+
+        i=random.uniform(0.0,0.5)
+
+        print(i)
---
+++ Latihan 2.py
@@ -0,0 +1,20 @@
+max=0
+
+while True:
+
+        a=int(input("Masukkan Bilangan = "))
+
+        print("")
+
+        if max < a:
+
+                max = a
+
+        print("Bilangan Terbesar = ",max)
+
+        if a==0:
+
+                break
+
+        print("")
+
